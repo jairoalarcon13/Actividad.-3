58ec4cb4746675205d4d55da557b8ed5c8b7d110
jairoalarcon13 <36417692+jairoalarcon13@users.noreply.github.com>
2018-04-24 07:18:28 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+// Vanilla JavaScript
+//const input = document.querySelector('input');
+//const h1 = document.querySelector('h1');
+//input.addEventListener('Keyup', () => h1.innerHTML = input.value);
+
+
+const vm = new Vue({
+	el:'main',
+	data:{
+		Usuario: 'John Blanco',
+		Registro: true,
+		edad: 11,
+
+
+	}
+});
